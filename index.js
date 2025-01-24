b618c68aca0f983d0385efc7f6943ab7f52e8647
husamXA1 <husamxm0@gmail.com>
2025-01-24 09:11:50 +0200

    update server port configuration to use environment variable

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello World" })
 })
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-    console.log("http://localhost:3000")
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 })
